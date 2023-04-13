5d4618bdf2d012c03d4e12bd216bf5a6ccc0fb0f
lorenzo.tomazzoni <lorenzo.tomazzoni@marconirovereto.it>
2023-04-13 17:31:13 +0200

    creazione primi componenti HOME/TEC/SCIENZA/SPORT

--- notiziario/src/app/app.module.ts
+++ notiziario/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SportComponent } from './sport/sport.component';
+import { ScienzaComponent } from './scienza/scienza.component';
+import { TecComponent } from './tec/tec.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SportComponent,
+    ScienzaComponent,
+    TecComponent
   ],
   imports: [
     BrowserModule,
---
+++ notiziario/src/app/scienza/scienza.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scienza',
+  templateUrl: './scienza.component.html',
+  styleUrls: ['./scienza.component.css']
+})
+export class ScienzaComponent {
+
+}
